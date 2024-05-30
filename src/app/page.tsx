8b15e3fcf2d1091108379a9459f8ec943d7bafcd
Werbeth Araujo <werbeth1991pbs@gmail.com>
2024-05-30 16:31:32 -0300

    feat: section how it works done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero";
+import HowItWorks from "@/components/HowItWorks";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
+      <HowItWorks />
     </>
   );
 }
